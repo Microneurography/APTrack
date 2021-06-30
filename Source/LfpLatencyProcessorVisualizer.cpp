06e12aca0947a692d6ee443c1e74b5c35d5246c9
jamosully <osullivan.james140@gmail.com>
2021-06-30 10:56:32 +0100

    Added track and sample number

--- Source/LfpLatencyProcessorVisualizer.cpp
+++ Source/LfpLatencyProcessorVisualizer.cpp
@@ -288,6 +288,10 @@ void LfpLatencyProcessorVisualizer::processTrack()
 				auto time = clock.toString(false, true, true, false);
 				auto string_time = time.toStdString();
 				processor->addMessage(string_time + 
+					" Current Sample Number: " + 
+					to_string(processor->currentSample) + 
+					" Current Track Number: " + 
+					to_string(processor->currentTrack) + 
 					" SPIKE FOUND Location: " + 
 					to_string(spikeLocations[i].searchBoxLocation) + 
 					"StartingSample: " + 
